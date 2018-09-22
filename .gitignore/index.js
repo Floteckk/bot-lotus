50c103abd616cb83e09e93a6af516e832ed71fd1
Floteckk <43255189+Floteckk@users.noreply.github.com>
2018-09-23 00:02:34 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js')
 const client = new Discord.Client()
 const prefix = 'Lotus'
 
-client.login('NDg5OTE0MzQ3NTQwMzE2MTkw.DnyvOQ.0n4LtJ2KYSM3g7sKZUHsHfN-eYE')
+client.login(process.en.TOKEN)
 
 client.on('guildMemberAdd', async member => {
 const channel = member.guild.channels.find('name', 'général');
